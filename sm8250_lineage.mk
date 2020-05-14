f7fba89381abc89b1ddf87085572a9b38244f451
Jesse Chan <jc@lineageos.org>
2020-05-14 23:31:56 +0800

    sm8250: switch to Snap Camera
    Signed-off-by: Jesse Chan <jc@lineageos.org>

--- sm8250_lineage.mk
+++ sm8250_lineage.mk
@@ -3,6 +3,12 @@ PRODUCT_PACKAGE_OVERLAYS += $(COMMON_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(COMMON_PATH)/overlay-lineage/lineage-sdk
 
+# Camera
+PRODUCT_PACKAGES += \
+    Snap
+
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(COMMON_PATH)/overlay-lineage/packages/apps/Snap
+
 # Fingerprint Inscreen
 PRODUCT_PACKAGES += \
     vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.samsung-sm8250
