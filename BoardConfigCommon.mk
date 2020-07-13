6642aa864859ccf797acfc2f40a4c35058e8a387
Nolen Johnson <johnsonnolen@gmail.com>
2020-07-12 23:36:23 -0400

    sm8250-common: Kill deprecated/unneeded board flags
    * TARGET_KERNEL_ARCH - no-op unless it differs from TARGET_ARCH.
    * TARGET_KERNEL_HEADER_ARCH - no-op unless it differs from TARGET_ARCH.
    * TARGET_LINUX_KERNEL_VERSION - Vendor specific.
    
    Change-Id: I8d140fecdbbaac979bc8398745ffa95b911e4c89

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -54,9 +54,6 @@ TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
 # Kernel config
 TARGET_KERNEL_CONFIG        := vendor/kona-sec-perf_defconfig 
 TARGET_KERNEL_SOURCE        := kernel/samsung/sm8250
-TARGET_KERNEL_ARCH          := arm64
-TARGET_KERNEL_HEADER_ARCH   := arm64
-TARGET_LINUX_KERNEL_VERSION := 4.19
 
 # Kernel flags
 BOARD_KERNEL_IMAGE_NAME        := Image
