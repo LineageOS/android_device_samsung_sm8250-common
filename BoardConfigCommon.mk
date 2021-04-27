9d871f366371841f6a658752b20c512a5fe495f5
Alexander Koskovich <zvnexus@outlook.com>
2021-04-27 06:57:55 -0700

    sm8250-common: Remove virtual framebuffer
     * It's largely for testing purposes and it consumes
       a large amount of memory. If your hardware doesn't
       have graphics then it can be useful, which isn't the
       case for shipped devices.
    
    Change-Id: I2348718ad3afba846498572161fa7e83dd8b7ce9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -67,8 +67,7 @@ BOARD_KERNEL_CMDLINE := \
     msm_rtb.filter=0x237 \
     printk.devkmsg=on \
     service_locator.enable=1 \
-    swiotlb=2048 \
-    video=vfb:640x400,bpp=32,memsize=3072000
+    swiotlb=2048
 BOARD_KERNEL_BASE          := 0x00000000
 BOARD_KERNEL_PAGESIZE      := 4096
 
