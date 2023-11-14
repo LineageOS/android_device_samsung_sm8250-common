090df3248d79a6523e09f8df2d53b166bcdcc46e
LuK1337 <priv.luk@gmail.com>
2023-11-14 20:17:10 +0100

    sm8250-common: extract-files: Prevent adding duplicated dependencies
    Change-Id: I186c38583670d509d4e0a0c89bc682706036f1ea

--- extract-files.sh
+++ extract-files.sh
@@ -78,7 +78,7 @@ function blob_fixup() {
             ;;
         vendor/lib64/libsensorlistener.so)
             [ "$2" = "" ] && return 0
-            "${PATCHELF}" --add-needed "libshim_sensorndkbridge.so" "${2}"
+            grep -q "libshim_sensorndkbridge.so" "${2}" || "${PATCHELF}" --add-needed "libshim_sensorndkbridge.so" "${2}"
             ;;
         vendor/lib64/hw/com.qti.chi.override.so)
             [ "$2" = "" ] && return 0
