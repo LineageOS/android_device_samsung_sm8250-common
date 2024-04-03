ccf5d710fc2cd0456c01dd8c40a1f1f020f2ec43
Tim Zimmermann <tim@linux4.de>
2024-04-03 05:23:41 +0200

    sm8250-common: Remove unneccessary keylayout
    * key 143 is already KEY_WAKEUP which is enough for dt2w
    
    Change-Id: I8c20415e3a3c104e643377a610b6aaebb4ab7956

--- common.mk
+++ common.mk
@@ -227,10 +227,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
-# Keylayout
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/keylayout/sec_touchscreen.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/sec_touchscreen.kl
-
 # Kernel
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
