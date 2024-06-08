ae861cfa80a8bbd362d0708919fa0fbbe3e994a1
someone5678 <someone5678@users.noreply.github.com>
2024-06-08 22:59:05 +0900

    sm8250-common: Use QCOM USB gadget HAL
    * Qualcomm enables it for kona targets
    * Import usb_compositions.conf for later customization
    
    Change-Id: Ib3383bc1173db22c539b2073d72419c42fcf92cc

--- common.mk
+++ common.mk
@@ -443,8 +443,12 @@ PRODUCT_PACKAGES += \
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti \
+    android.hardware.usb.gadget@1.2-service-qti \
     init.qcom.usb.rc \
     init.qcom.usb.sh
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/usb/usb_compositions.conf:$(TARGET_COPY_OUT_VENDOR)/etc/usb_compositions.conf
 
 # Tether
 PRODUCT_PACKAGES += \
