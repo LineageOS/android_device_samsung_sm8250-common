46e35c60684fde575096becdcbda3977e01df9e2
Michael Bestas <mkbestas@gmail.com>
2024-09-11 00:04:13 +0300

    sm8250-common: Shim widevine with libcrypto_shim
    The following change
    https://github.com/google/boringssl/commit/e202e51cb0912f36dafbd2e67cf04d6ec82f3180
    removed symbols that our libs require.
    
    Change-Id: I1325c34ecec3d339dd68c665010ecb36fa2e6a29

--- extract-files.sh
+++ extract-files.sh
@@ -70,6 +70,10 @@ done
 
 function blob_fixup() {
     case "${1}" in
+        vendor/lib/libwvhidl.so|vendor/lib/mediadrm/libwvdrmengine.so)
+            [ "$2" = "" ] && return 0
+            grep -q "libcrypto_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
+            ;;
         vendor/lib64/libarcsoft_multi_frame_video_hdr.so)
             [ "$2" = "" ] && return 0
             "${PATCHELF_0_17_2}" --clear-symbol-version "remote_handle_close" "${2}"
