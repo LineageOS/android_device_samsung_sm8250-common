8fb07fed79f86b9568aee3779cd2a554922e8b4f
Bruno Martins <bgcngm@gmail.com>
2024-09-28 18:31:06 +0100

    sm8250-common: Resolve missing crypto symbols
    Change-Id: I8c3efa2123155898d63644d82baabbd0f767b350

--- extract-files.sh
+++ extract-files.sh
@@ -72,7 +72,7 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             grep -q "libshim_sensorndkbridge.so" "${2}" || "${PATCHELF}" --add-needed "libshim_sensorndkbridge.so" "${2}"
             ;;
-        vendor/lib64/hw/gatekeeper.mdfpp.so|vendor/lib64/libskeymaster4device.so)
+        vendor/lib64/hw/gatekeeper.mdfpp.so|vendor/lib64/libkeymaster_helper.so|vendor/lib64/libskeymaster4device.so)
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libcrypto.so" "libcrypto-v33.so" "${2}"
             ;;
