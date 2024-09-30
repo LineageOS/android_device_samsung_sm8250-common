5de433da013800db664f1574bd61a4b206b2e9df
Andreas Schneider <asn@cryptomilk.org>
2024-09-30 23:04:48 +0200

    sm8250-common: Replace prebuilt libsensorndkbridge with a shim
    Change-Id: Id94ed43336e84934f42f56c58e506b50af3c59fd

--- extract-files.sh
+++ extract-files.sh
@@ -80,6 +80,10 @@ function blob_fixup() {
             xxd -p -c0 "${2}" | sed "s/600e40f9820c805224008052e10315aa080040f9e30314aa/600e40f9820c805224008052e10315aa080040f9030080d2/g" | xxd -r -p > "${2}".patched
             mv "${2}".patched "${2}"
             ;;
+        vendor/lib64/libsensorlistener.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --add-needed "libshim_sensorndkbridge.so" "${2}"
+            ;;
         vendor/lib64/hw/com.qti.chi.override.so)
             [ "$2" = "" ] && return 0
             xxd -p "${2}" | tr -d \\n > "${2}".hex
