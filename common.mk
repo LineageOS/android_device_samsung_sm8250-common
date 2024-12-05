4e26b7aacde9558bb9518bd0cf12aab1a5623e1b
LuK1337 <priv.luk@gmail.com>
2024-12-05 16:41:51 +0100

    sm8250-common: Build libqti_vndfwk_detect.vendor:32
    Before:
    audio_hw_extn: spkr_prot_feature_init: Called with feature Enabled, vendor_enhanced_info 0x0
    
    After:
    audio_hw_extn: spkr_prot_feature_init: Called with feature Enabled, vendor_enhanced_info 0x3
    
    Change-Id: Ie5c33eafc1e0be2eeda8e0a6bbbc3712e32986e9

--- common.mk
+++ common.mk
@@ -283,6 +283,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# QTI fwk-detect
+PRODUCT_PACKAGES += \
+    libqti_vndfwk_detect.vendor:32
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.samsung-multihal
