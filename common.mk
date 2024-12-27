0311ba458467148cd20be944c4e63ffd54de756b
Bruno Martins <bgcngm@gmail.com>
2024-12-27 14:34:02 +0000

    sm8250-common: Switch to QTI USB HAL
    Change-Id: I9e3aeaead8ee7e0d7ce9d50e95f6ea47bf025cd8

--- common.mk
+++ common.mk
@@ -442,9 +442,12 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb-service.samsung \
+    android.hardware.usb@1.3-service-qti \
     init.qcom.usb.rc \
     init.qcom.usb.sh
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/opensource/usb/etc
 
 # Tether
 PRODUCT_PACKAGES += \
