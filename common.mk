f37a46805d059063a9e330042faa65b4f4dd2d77
Bruno Martins <bgcngm@gmail.com>
2024-12-28 13:05:33 +0000

    sm8250-common: audio: Drop legacy effects configuration
    Change-Id: Ie29c42f54281ec9df2760612716e72ddd456b1f8

--- common.mk
+++ common.mk
@@ -95,7 +95,6 @@ PRODUCT_PACKAGES += \
 TARGET_EXCLUDES_AUDIOFX := true
 
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/audio/configs/audio_effects.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.conf \
     $(COMMON_PATH)/audio/configs/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(COMMON_PATH)/audio/configs/audio_io_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_io_policy.conf\
     $(COMMON_PATH)/audio/configs/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
