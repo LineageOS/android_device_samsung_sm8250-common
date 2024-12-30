8891688874905fd3e82626d136a5e632dbdbb859
Atakan <ata.kaner03@gmail.com>
2024-12-30 11:44:58 +0300

    sm8250-common: Update blobs and sync init with G780GXXSDEXJ3
    Just straight update, decommonization and clean up to follow.
    
    Change-Id: Ibdef25cd01b502a4911b8e997ecb7dc98523737b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -192,7 +192,7 @@ TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_samsung_sm8250
 TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)
 
 # Security patch
-VENDOR_SECURITY_PATCH := 2024-05-01
+VENDOR_SECURITY_PATCH := 2024-10-01
 
 # SePolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
