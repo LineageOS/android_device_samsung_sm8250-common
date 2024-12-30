a703897d1b1cff627f228f51e73ba23edb11be43
Atakan <ata.kaner03@gmail.com>
2024-12-30 18:49:04 +0300

    sm8250-common: Convert recovery.qcom.rc to prebuilt_etc
    Change-Id: I07556e691d2a3b1c465f8bd8e59b3bffb391cfd3

--- common.mk
+++ common.mk
@@ -46,6 +46,7 @@ PRODUCT_PACKAGES += \
     init.nfc.samsung.rc \
     init.qcom.rc \
     init.ramplus.rc \
+    init.recovery.qcom.rc \
     init.samsung.bsp.rc \
     init.samsung.display.rc \
     init.samsung.eif.rc \
@@ -382,10 +383,6 @@ PRODUCT_PACKAGES += \
     libsqlite.vendor \
     libvndfwk_detect_jni.qti \
     libvndfwk_detect_jni.qti.vendor
-
-# Recovery
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/recovery/root/init.recovery.qcom.rc:root/init.recovery.qcom.rc
 
 # RIL
 PRODUCT_PACKAGES += \
