6ab460f04bce3f6a4c1b0fa2787ca019a6e802c5
Michael Bestas <mkbestas@lineageos.org>
2025-01-01 03:50:53 +0200

    sm8250-common: Update display flags & packages
    Required after display HAL blueprint conversion
    
    Change-Id: Ifc4e10703d3424607b9dc8bcc8e417d1c88cc2f3

--- common.mk
+++ common.mk
@@ -142,7 +142,6 @@ PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
-    gralloc.kona \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.composer-service \
     vendor.qti.hardware.memtrack-service
