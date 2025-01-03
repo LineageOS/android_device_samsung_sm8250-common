541a7a9e0c7c857c02dad557251f160d862bb0d5
Bruno Martins <bgcngm@gmail.com>
2025-01-03 18:05:01 +0000

    sm8250-common: Reorganize and clean up blobs
    Change-Id: Ifda991561e4422bd7f44d0b66f9bf49b52c55634

--- common.mk
+++ common.mk
@@ -134,12 +134,9 @@ $(call soong_config_set,samsungCameraVars,needs_sec_reserved_field,true)
 
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider-service.samsung \
-    libgrallocusage.vendor \
+    libpng.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor \
-    libgui_vendor \
-    libcamera2ndk_vendor \
-    libpng.vendor
 
 # Configstore
 PRODUCT_PACKAGES += \
@@ -166,7 +163,6 @@ PRODUCT_PACKAGES += \
     libdisplayconfig.system.qti \
     libqdMetaData \
     libqdMetaData.system \
-    libtinyxml \
     libtinyxml2 \
     vendor.display.config@1.0 \
     vendor.display.config@1.0.vendor \
