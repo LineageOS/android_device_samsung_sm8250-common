d70d1cc8c05e6aa63a0b11103893e96977ff9aed
Bruno Martins <bgcngm@gmail.com>
2025-01-04 23:25:58 +0000

    sm8250-common: Work around using prebuilt audio primary HAL
    Change-Id: Iff2fc236e8a551a591a00f7562a84cb845fe86c8

--- common.mk
+++ common.mk
@@ -89,6 +89,7 @@ PRODUCT_PACKAGES += \
     libhdmiedid \
     libhfp \
     libldnhncr \
+    libprocessgroup.vendor \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
