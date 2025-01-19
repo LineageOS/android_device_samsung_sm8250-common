LineageOS/android_device_oneplus_sm8250-common

sm8250-common: Shim widevine with libcrypto_shim
The following change
https://github.com/google/boringssl/commit/e202e51cb0912f36dafbd2e67cf04d6ec82f3180
removed symbols that our libs require.

Change-Id: I1325c34ecec3d339dd68c665010ecb36fa2e6a29

extract-files.sh
@@ -84,6 +84,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${SIGSCAN}" -p "72 1F 00 94" -P "1F 20 03 D5" -f "${2}"
             ;;
+        odm/lib64/libwvhidl.so|odm/lib64/mediadrm/libwvdrmengine.so)
+            [ "$2" = "" ] && return 0
+            grep -q "libcrypto_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
+            ;;
         product/app/PowerOffAlarm/PowerOffAlarm.apk)
             [ "$2" = "" ] && return 0
             apktool_patch "${2}" "${MY_DIR}/blob-patches/PowerOffAlarm.patch" -s

LineageOS/android_device_samsung_sm8250-common

extract-files.sh
@@ -70,6 +70,10 @@ done
 
 function blob_fixup() {
     case "${1}" in
+        vendor/lib/libwvhidl.so|vendor/lib/mediadrm/libwvdrmengine.so)
+            [ "$2" = "" ] && return 0
+            grep -q "libcrypto_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
+            ;;
         vendor/lib64/libarcsoft_multi_frame_video_hdr.so)
             [ "$2" = "" ] && return 0
             "${PATCHELF_0_17_2}" --clear-symbol-version "remote_handle_close" "${2}"