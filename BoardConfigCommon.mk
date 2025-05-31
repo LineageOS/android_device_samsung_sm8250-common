7d2c7c5d6bde1cd07e9106f0763037db88ece6bf
Bruno Martins <bgcngm@gmail.com>
2025-05-31 13:01:12 +0100

    sm8250-common: Move and recategorize compat matrix and manifest flags
    Qualcomm also sets these in (common) product makefiles.
    
    Change-Id: I7139cba92454106fbca5f6105b966a43e70fe9d6

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -135,17 +135,6 @@ AUDIO_FEATURE_ENABLED_PROXY_DEVICE := true
 AUDIO_FEATURE_ENABLED_SSR := false
 BOARD_SUPPORTS_SOUND_TRIGGER := true
 
-# HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
-DEVICE_MANIFEST_FILE += $(COMMON_PATH)/configs/manifest.xml
-ifneq ($(TARGET_IS_WIFI-ONLY),true)
-DEVICE_MANIFEST_FILE += $(COMMON_PATH)/configs/network_manifest.xml
-endif
-DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
-
 # Media
 TARGET_USES_ION := true
 
