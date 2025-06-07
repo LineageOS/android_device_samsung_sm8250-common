d842d22292d54d5ca6c49741a3da712c88ca4c29
Bruno Martins <bgcngm@gmail.com>
2025-06-07 21:28:56 +0100

    sm8250-common: touch: Migrate to AIDL
    Change-Id: Ibbac9b4320fc64c5b6f365b8e9bd977d71d0d4dd

--- touch/TouchscreenGesture.cpp
+++ touch/TouchscreenGesture.cpp
@@ -1,30 +1,20 @@
 /*
- * Copyright (C) 2019 The LineageOS Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * SPDX-FileCopyrightText: 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <fstream>
+#define LOG_TAG "vendor.lineage.touch-service.samsung_sm8250"
 
 #include "TouchscreenGesture.h"
 
+#include <android-base/file.h>
+
+using ::android::base::WriteStringToFile;
+
+namespace aidl {
 namespace vendor {
 namespace lineage {
 namespace touch {
-namespace V1_0 {
-namespace samsung {
-
-static constexpr const char* kTspPath = TSP_CMD_NODE;
 
 const std::map<int32_t, TouchscreenGesture::GestureInfo> TouchscreenGesture::kGestureInfoMap = {
     // clang-format off
@@ -32,36 +22,26 @@ const std::map<int32_t, TouchscreenGesture::GestureInfo> TouchscreenGesture::kGe
     // clang-format on
 };
 
-bool TouchscreenGesture::isSupported() {
-    std::ifstream file(kTspPath);
-    return file.good();
-}
-
-// Methods from ::vendor::lineage::touch::V1_0::ITouchscreenGesture follow.
-Return<void> TouchscreenGesture::getSupportedGestures(getSupportedGestures_cb resultCb) {
+ndk::ScopedAStatus TouchscreenGesture::getSupportedGestures(std::vector<Gesture>* _aidl_return) {
     std::vector<Gesture> gestures;
 
     for (const auto& entry : kGestureInfoMap) {
         gestures.push_back({entry.first, entry.second.name, entry.second.keycode});
     }
-    resultCb(gestures);
 
-    return Void();
+    *_aidl_return = gestures;
+    return ndk::ScopedAStatus::ok();
 }
 
-Return<bool> TouchscreenGesture::setGestureEnabled(
-    const ::vendor::lineage::touch::V1_0::Gesture&, bool enabled) {
-    std::fstream file(kTspPath);
+ndk::ScopedAStatus TouchscreenGesture::setGestureEnabled(const Gesture& gesture, bool enabled) {
+    if (!WriteStringToFile(enabled ? "singletap_enable,1" : "singletap_enable,0", TSP_CMD_NODE)) {
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+    }
 
-    file << "singletap_enable," << (enabled ? "1" : "0");
-
-    return !file.fail();
+    return ndk::ScopedAStatus::ok();
 }
 
-// Methods from ::android::hidl::base::V1_0::IBase follow.
-
-}  // namespace samsung
-}  // namespace V1_0
 }  // namespace touch
 }  // namespace lineage
 }  // namespace vendor
+}  // namespace aidl
