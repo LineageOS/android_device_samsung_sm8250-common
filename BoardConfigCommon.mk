49c83b5156e6c8ed45d35a4681ac96ec174ede75
Bruno Martins <bgcngm@gmail.com>
2025-08-03 22:37:58 +0100

    sm8250-common: Inherit SEC common.config fragment
    Change-Id: Ia30ae5f2facf22415a4f77310fcf0da285eb6264

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -74,8 +74,9 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
 TARGET_KERNEL_SOURCE := kernel/samsung/sm8250
 TARGET_KERNEL_CONFIG := \
-    vendor/kona-sec-perf_defconfig \
-    vendor/debugfs.config
+    vendor/kona-perf_defconfig \
+    vendor/debugfs.config \
+    vendor/samsung/kona-sec-common.config
 
 # Additional root folders
 TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
