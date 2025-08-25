75e508cb6c1ec64ef04301ac7df8f01c64c4d036
Atakan <ata.kaner03@gmail.com>
2025-08-25 13:29:32 +0300

    sm8250-common: Support Dolby Atmos encoders
    Test: Boot, open Apple Music. Apple Music shows Spatial Audio option. Play any song with Atmos support, player shows Dolby Atmos logo.
    
    Change-Id: I678574a5101e3a8b9dd7b28e1e1d8f55db53e80b

--- extract-files.py
+++ extract-files.py
@@ -47,6 +47,14 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/libsec-ril.so': blob_fixup()
         .binary_regex_replace(b'ril.dds.call.ongoing', b'vendor.calls.slot_id')
         .sig_replace('60 0E 40 F9 E1 03 15 AA 82 0C 80 52 E3 03 14 AA', '60 0E 40 F9 E1 03 15 AA 82 0C 80 52 30 08 0D D2'),
+    'vendor/bin/hw/samsung.software.media.c2@1.0-service': blob_fixup()
+        .replace_needed('libavservices_minijail_vendor.so', 'libavservices_minijail.so'),
+    'vendor/etc/vintf/manifest/sec_c2_manifest_default0.xml': blob_fixup()
+        .regex_replace('default', 'default0'),
+    'vendor/lib64/libSecC2ComponentStore.so': blob_fixup()
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-v30.so'),
+    'vendor/lib64/libcodec2_vndk-v30.so': blob_fixup()
+        .add_needed('libui_shim.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
