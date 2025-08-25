4303190dd1fd48ddfcf8c066b226828d482eb83b
Atakan <ata.kaner03@gmail.com>
2025-08-25 13:29:32 +0300

    sm8250-common: Support Dolby Atmos decoders
    Test: Boot, open Apple Music. Apple Music shows Spatial Audio option. Play any song with Atmos support, player shows Dolby Atmos logo.
    
    Change-Id: I678574a5101e3a8b9dd7b28e1e1d8f55db53e80b

---
+++ shims/libcodec2_vndk/C2Buffer.cpp
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2025 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <C2BufferPriv.h>
+
+extern "C" void
+_ZN17C2PooledBlockPoolC1ERKNSt3__110shared_ptrI11C2AllocatorEEmNS_13BufferPoolVerE(
+    const std::shared_ptr<C2Allocator> &allocator,
+    const C2BlockPool::local_id_t localId,
+    C2PooledBlockPool::BufferPoolVer ver);
+
+extern "C" void _ZN17C2PooledBlockPoolC1ERKNSt3__110shared_ptrI11C2AllocatorEEm(
+    const std::shared_ptr<C2Allocator> &allocator,
+    const C2BlockPool::local_id_t localId) {
+  _ZN17C2PooledBlockPoolC1ERKNSt3__110shared_ptrI11C2AllocatorEEmNS_13BufferPoolVerE(
+      allocator, localId, C2PooledBlockPool::VER_HIDL);
+}
