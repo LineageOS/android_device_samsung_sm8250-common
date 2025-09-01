ea95d708d341bb18742ef4c06cc5f753156a20dc
Bruno Martins <bgcngm@gmail.com>
2025-09-01 01:43:37 +0100

    sm8250-common: Migrate to LiveDisplay AIDL HAL
    Change-Id: Iafa206bb882cc49866a4b94b3ac03f15c4399c30

--- common.mk
+++ common.mk
@@ -230,7 +230,7 @@ $(call soong_config_set,lineage_health,fast_charge_value_fast_charge,0)
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service.samsung-qcom
+    vendor.lineage.livedisplay-service.samsung-qcom
 
 # Media
 PRODUCT_COPY_FILES += \
