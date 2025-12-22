3ce2c64d3c26ba65a1e58308bf82399244373c81
Atakan <ata.kaner03@gmail.com>
2025-12-22 23:03:54 +0300

    [DNM] sm8250-common: SamsungBiometrics testing
    Change-Id: I69192354afc05a39db4583963b3801a3d2fc05d3

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -66,7 +66,8 @@ BOARD_KERNEL_CMDLINE := \
     msm_rtb.filter=0x237 \
     printk.devkmsg=on \
     service_locator.enable=1 \
-    swiotlb=2048
+    swiotlb=2048 \
+    androidboot.selinux=permissive
 BOARD_KERNEL_BASE          := 0x00000000
 BOARD_KERNEL_PAGESIZE      := 4096
 
