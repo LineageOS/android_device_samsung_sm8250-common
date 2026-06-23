69c01402b47122e12f5391b154f90d7ac4967805
Michael Bestas <mkbestas@lineageos.org>
2026-06-24 01:57:14 +0300

    sm8250-common: power-libperfmgr: Update included soong namespaces
    We're putting everything that depends on pixelatoms behind a soong
    namespace, since hardware/google/pixel is no longer available after
    CP2A. Using a combination of prebuilts and source built things that
    depend on pixelatoms makes the build fail catastrophically, so we
    will just make everything prebuilt for supported Pixel devices.
    
    Change-Id: I3d9e1d9436f1edfbb45bca0aa709334287bb12ce

--- common.mk
+++ common.mk
@@ -384,7 +384,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
-    hardware/google/pixel \
+    hardware/google/pixel/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client \
     hardware/samsung \
     vendor/qcom/opensource/usb/etc
