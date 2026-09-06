ceaa184c38e290739cd64f5c6dee6cb346778b16
Thomas Topfstedt <contato.thomastop@gmail.com>
2026-09-05 23:46:53 -0300

    sm8250-common: Disable broken StrongBox implementation
    The QTI StrongBox Keymaster service never returns from key generation on
    Android 16. This leaves keystore2 requests pending indefinitely and
    blocks applications including Tailscale, OpenVPN, and Onleihe.
    
    Stop advertising the StrongBox feature, remove its VINTF instance, and
    do not ship the broken service until the implementation is functional
    again. Keep the shared Keymaster libraries used by the default
    implementation.
    
    Test: Tailscale cold start, login, tunnel setup,
          and 5/5 VPN pings succeeded with no StrongBox watchdog entries
    Bug: https://github.com/LineageOS/issues/issues/274
    Change-Id: I14babe314b25512e35c6518d9ece89314997dda7

--- common.mk
+++ common.mk
@@ -266,7 +266,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.light.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepcounter.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml \
-    frameworks/native/data/etc/android.hardware.strongbox_keystore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.strongbox_keystore.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml \
